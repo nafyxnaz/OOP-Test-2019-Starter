06582610819f9c055257e37a3c3a887144c80293
Nafeesa <nafyxnaz@gmail.com>
2019-04-10 17:07:00 +0100

    resistor class

---
+++ src/ie/tudublin/resistor.java
@@ -0,0 +1,15 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+
+import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
+
+public class Resistor
+{
+    public void value (int value);
+    public void ones (int ones);
+    public void tens (int tens);
+    public void hundreds (int hundreds); 
+}
