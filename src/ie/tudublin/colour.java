b70c8889201896379b8b96f1ee08794d1d790ec3
Nafeesa <nafyxnaz@gmail.com>
2019-04-10 16:36:06 +0100

    encapsulating data

--- src/ie/tudublin/colour.java
+++ src/ie/tudublin/colour.java
@@ -1,5 +1,16 @@
 package ie.tudublin;
+import java.util.ArrayList;
 
 public class colour {
-    
-}
+
+    public void black (int black)
+    {
+        this.black = black;
+    }
+
+    public int getNumblack()
+    {
+        return getNumblack();
+    }
+
+  
